ab4084ca6252cb2fef33911fc51d5e2e0f6f93d9
DaPasta <davidandrewbaum@gmail.com>
2019-03-30 01:26:34 -0700

    initial commit

---
+++ src/main/java/com/nextfolio/server/NextserverApplication.java
@@ -0,0 +1,13 @@
+package com.nextfolio.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextserverApplication.class, args);
+    }
+
+}
