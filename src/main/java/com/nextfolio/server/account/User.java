870766cebf205df563da62ed1e8628f32b073020
889884m <889884m@gmail.com>
2019-03-30 14:13:54 -0700

    testing 500 server error

--- src/main/java/com/nextfolio/server/account/User.java
+++ src/main/java/com/nextfolio/server/account/User.java
@@ -4,6 +4,10 @@ public class User {
     private String username;
     private String password;
 
+    public User() {
+        
+    }
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
